110c6df5c9efd4ec4eda173d345f76c87da10ba9
Mehrab HP <white_joker20@yahoo.com>
2024-12-01 21:29:15 +0300

    fix: update favicon

--- app/root.tsx
+++ app/root.tsx
@@ -58,6 +58,10 @@ export const links: Route.LinksFunction = () => [
 		rel: "stylesheet",
 		href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
 	},
+	{
+		rel: 'icon',
+		href: '/ultimate-x-logo.png'
+	},
 	{ rel: "stylesheet", href: stylesheet },
 ];
 
